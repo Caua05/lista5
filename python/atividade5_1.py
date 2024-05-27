3ab1d9a171f19ec903da3d0fc1fb7e319ac80fd9
Caua05 <162156499+Caua05@users.noreply.github.com>
2024-05-27 12:30:04 -0300

    Add files via upload

---
+++ python/atividade5_1.py
@@ -0,0 +1,12 @@
+pessoa = {
+    "nome": "Cauã",
+    "sobrenome" : "Ferreira",
+    "idade" : 19,
+    "cidade" :"Campina Grande", 
+}
+
+nome_da_pessoa = pessoa["nome"]
+sobrenome_da_pessoa = pessoa["sobrenome"]
+idade_da_pessoa = pessoa["idade"]
+cidade_da_pessoa = pessoa["cidade"]
+print( pessoa["nome"],pessoa["sobrenome"], pessoa["idade"] , pessoa["cidade"] )
---
+++ python/atividade5_2.py
@@ -0,0 +1,14 @@
+numeros_favoritos={
+    "aninha",66,
+    "paulo",9,
+    "caua",10,
+    "geovanni",35,
+    "juan",5
+}
+numeros_favorito1={'aninha':'66'}
+numeros_favorito2={'paulo':'9'}
+numeros_favorito3={'caua':'10'}
+numeros_favorito4={'geovanni':'35'}
+numeros_favorito5={'juan':'5'}
+
+print(numeros_favorito1 , numeros_favorito2 , numeros_favorito3 , numeros_favorito4 , numeros_favorito5)
